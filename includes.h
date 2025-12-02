0be7c2ad97c3139fa940250fdfb67fd9c9c5bcd2
knoppi1707 <134884422+knoppi1707@users.noreply.github.com>
2025-12-02 08:45:37 +0100

    Update includes.h

--- includes.h
+++ includes.h
@@ -1,9 +1,4 @@
 #pragma
-
-#include "../imgui/imgui.h"
-#include "../imgui/imgui_impl_win32.h"
-#include "../imgui/imgui_impl_dx11.h"
-#include <d3d11.h>
 #include <tchar.h>
 #include <windows.h>
 #include <tlhelp32.h>
@@ -23,10 +18,8 @@
 #include <atomic>
 #include <cmath>
 #include <stdexcept>
-
 #include <iostream>
 #include <windows.h>
 #include <fstream>
 #include <string>
 #include <wininet.h>
-#pragma comment(lib, "wininet.lib")
