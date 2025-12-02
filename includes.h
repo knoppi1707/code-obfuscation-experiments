d35805a5f57841150ff58cd05c4a7610a253dec9
knoppi1707 <134884422+knoppi1707@users.noreply.github.com>
2025-12-02 08:45:10 +0100

    Add files via upload

---
+++ includes.h
@@ -0,0 +1,32 @@
+#pragma
+
+#include "../imgui/imgui.h"
+#include "../imgui/imgui_impl_win32.h"
+#include "../imgui/imgui_impl_dx11.h"
+#include <d3d11.h>
+#include <tchar.h>
+#include <windows.h>
+#include <tlhelp32.h>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <string.h>
+#include <array>
+#include <ctime>
+#include <thread>
+#include <map>
+#include <set>
+#include <mutex>
+#include <atomic>
+#include <cmath>
+#include <stdexcept>
+
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <string>
+#include <wininet.h>
+#pragma comment(lib, "wininet.lib")
